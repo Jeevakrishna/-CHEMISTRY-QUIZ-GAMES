3cb4cc664c109c7f299c4e97742aa552ffcbc643
Jeeva Krishna <88268605+Jeevakrishna@users.noreply.github.com>
2021-08-08 19:40:59 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,138 @@
+let MCQS = [{
+             question: "Bauxite has the Composition ?",
+    choice1: "Al₂O₃",
+    choice2: "Fe2O3.2H2O",
+    choice3: "Al2O3.nH2O",
+    choice4: "none of these",
+    answer: 2
+},
+            {
+             question: "IUPAC Name of [Cr(NH3)3(H2O)3] Cl 3?",
+    choice1: "Tetraamminecarboncobalt(3)chloride",
+    choice2: "Diamminesilver(1)chloride",
+    choice3: "Triamminetriaquachromium(3)chloride",
+    choice4: "Pentaamminechloridocobalt(3)ion",
+    answer: 2
+},
+            {
+             question: "IUPAC name of the complex K3[Al(C2O4)3] is …………….",
+    choice1: " potassiumtrioxalatoaluminium (3)",
+    choice2: "potassiumtrioxalatoaluminate (2)",
+    choice3: "potassiumtrisoxalatoaluminium (2)",
+    choice4: "potassiumtrioxalatoaluminate (3)",
+    answer: 3
+},
+            {
+             question: "Which type of isomerism is exhibited by [Pt(NH3)2Cl2] ?",
+    choice1: "Geometrical isomerism",
+    choice2: "Linkage isomerism",
+    choice3: "Optical isomerism",
+    choice4: "Coordination  isomerism",
+    answer: 0
+},
+            {
+             question: "Formula of tris (ethane-1, 2-diamine) iron (II) phosphate …………?",
+    choice1: "[Fe(CH3 – CH(NH2)2)3] (PO4)3",
+    choice2: "[Fe(H2N – CH2 – CH2 – NH2)3](PO4)",
+    choice3: "[Fe(H2N – CH3 – CH3 – NH3)3](PO4)2",
+    choice4: "[Fe(H2N – CH2 – CH2 – NH2)3](PO4)2",
+    answer: 3
+},
+            {
+             question: "XeF6 on complete hydrolysis produces …………",
+    choice1: "XeOF4",
+    choice2: "XeO2F4",
+    choice3: "XeO3",
+    choice4: "XeO2",
+    answer: 3
+},
+            {
+               question: "Among the following, which is the strongest oxidizing agent?",
+    choice1: "Cl2",
+    choice2: "F2",
+    choice3: "Br2",
+    choice4: "I2",
+    answer: 1
+},{
+               question: "Among the following the correct order of acidity is …………?",
+    choice1: "HClO2 < HCIO < HClO3 < HClO4",
+    choice2: "HClO4 < HClO2 < HCIO < HClO3",
+    choice3: "HCIO < HClO2 < HClO3 < HClO4",
+    choice4: "HClO3 < HClO4 < HClO2 < HCIO",
+    answer: 2
+},
+            {
+               question: "When copper is heated with cone HNO3 it produces …………",
+    choice1: "CU(NO3)2 , NO and NO2",
+    choice2: "Cu(NO3)2 and N2O",
+    choice3: "CU(NO3)2 and NO2",
+    choice4: "Cu(NO3)2 and NO",
+    answer: 2
+},
+            {
+               question: "Crystal field stabilization energy for high spin d5 octahedral complex is…………….",
+    choice1: "– 0.6∆0",
+    choice2: "0",
+    choice3: "2 (P – ∆0)",
+    choice4: "2 (P + ∆0)",
+    
+    answer: 1
+},
+            {
+               question: "Solid CO2 is an example of ………..?",
+    choice1: "Covalent solid",
+    choice2: "Metallic solid",
+    choice3: "Molecular solid ",
+    choice4: "Ionic solid",
+    answer: 2
+            
+},
+             {
+               question: "The number of carbon atoms per unit cell of diamond is ………..?",
+    choice1: "4",
+    choice2: "1",
+    choice3: "8",
+    choice4: "6",
+    answer: 2
+            
+},
+            {
+             question: "The Crystal with a metal deficiency defect is ………..?",
+    choice1: "NaCI",
+    choice2: "FeO ",
+    choice3: "ZnO",
+    choice4: "KCI",
+    answer: 1
+            
+},
+       
+          {
+             question: " Which one of the following reaction represents calcination? ",
+    choice1: " 2Zn + O2 → 2ZnO ",
+    choice2: " 2ZnS + 3O2 → 2ZnO + 2SO2 ",
+    choice3: " MgCO3 → MgO + CO2",
+    choice4: "Both (a) and (c) ",
+    answer: 2
+            
+},
+           {
+             question: " Which of the metal is extracted by Hall-Herold process? ",
+    choice1: " Ni",
+    choice2: " Al ",
+    choice3: " Cu",
+    choice4: " Zn ",
+    answer: 1
+            
+},
+    
+  
+
+
+
+
+
+
+
+
+
+];
